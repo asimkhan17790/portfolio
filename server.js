b58ca67c50cbddcc8af61c7dd344cedd9ac52e3e
asimkhan17790 <asimkhan17790@gmail.com>
2017-03-04 15:07:06 -0500

    initial setup

--- server.js
+++ server.js
@@ -5,37 +5,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var passport      = require('passport');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-
-app.use(session({
-    secret: 'this is the secret',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
-
-require ("./test/app.js")(app);
-require("./todo/app")(app);
-require("./blog/app")(app);
-require('./experiments/upload/app')(app);
-// require('./experiments/mongoose/app')(app);
-
-// require("./assignment-wed/app.js")(app);
-
-require("./lectures/mongo/movies")(app);
-// require('./lectures-wed/mongo/movies')(app);
-require('./experiments/mongoose/projects/app')(app);
-
-var assignment = require("./assignment/app.js");
-assignment(app);
 
 var port      = process.env.PORT || 4000;
 
